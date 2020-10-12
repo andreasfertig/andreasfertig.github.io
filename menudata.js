74127d18b5f9de32c6a75f7322bc15fdb722f6c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-12 16:41:57 +0000

    Deploy andreasfertig/andreasfertig.github.io to github.com/andreasfertig/andreasfertig.github.io.git:master

--- menudata.js
+++ menudata.js
@@ -93,6 +93,7 @@ var menudata={children:[
 {text:"All",url:"globals.html",children:[
 {text:"b",url:"globals.html#index_b"},
 {text:"c",url:"globals.html#index_c"},
+{text:"f",url:"globals.html#index_f"},
 {text:"g",url:"globals.html#index_g"},
 {text:"h",url:"globals.html#index_h"},
 {text:"i",url:"globals.html#index_i"},
