45d61833c5d77aab13b8ec69d59b99766188e69e
andreasfertig <andy@cppinsights.io>
2022-07-19 09:48:15 +0000

    Deploying to main from main @ e60cdde48b4058ce1a69137544b00e9098c9a8e8

--- menudata.js
+++ menudata.js
@@ -118,6 +118,7 @@ var menudata={children:[
 {text:"i",url:"namespacemembers_func.html#index_i"},
 {text:"m",url:"namespacemembers_func.html#index_m"},
 {text:"n",url:"namespacemembers_func.html#index_n"},
+{text:"o",url:"namespacemembers_func.html#index_o"},
 {text:"r",url:"namespacemembers_func.html#index_r"},
 {text:"s",url:"namespacemembers_func.html#index_s"},
 {text:"t",url:"namespacemembers_func.html#index_t"}]},
